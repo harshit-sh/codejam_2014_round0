6aa5fbc61dc01cb9565a8fa1b3145103ced86cbb
supercr7 <hsharma1205@gmail.com>
2014-04-15 19:06:18 +0530

    Update magic_trick.cc

--- magic_1/magic_trick.cc
+++ magic_1/magic_trick.cc
@@ -11,12 +11,12 @@ using namespace std;
 vector<int> getCommon(vector<int> &v1, vector<int> &v2){
 	vector<int> v3;
 
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
+	sort(v1.begin(), v1.end());
+    	sort(v2.begin(), v2.end());
 
-    set_intersection(v1.begin(),v1.end(),v2.begin(),v2.end(),back_inserter(v3));
+    	set_intersection(v1.begin(),v1.end(),v2.begin(),v2.end(),back_inserter(v3));
 
-    return v3;
+    	return v3;
 } 
 
 
@@ -26,9 +26,9 @@ int main(){
 	input_file.open("A-small-attempt3.in");
 	output_file.open("C-small-3-magic.out");
 	int t = 0;
-//	cin >> t;
 	input_file >> t;
 	int k = 1;
+	
 	while(t){
 		int r1 = 0;
 		int r2= 0;
@@ -36,11 +36,9 @@ int main(){
 		vector<int> v2;
 		int A[4][4];
 		int B[4][4];
-//		cin >> r1;
 		input_file >> r1;
 		for( int i = 0 ; i < 4; i++){
 			for(int j = 0; j < 4; j++){
-		//		cin >> A[i][j];
 				input_file >> A[i][j];
 			}
 		}
@@ -48,11 +46,10 @@ int main(){
 		for (int j = 0; j < 4; j++){
 			v1.push_back( A[real_r1][j] );
 		}
-	//	cin >> r2;
+		
 		input_file >> r2;
 		for( int i = 0 ; i < 4; i++){
 			for(int j = 0; j < 4; j++){
-			//	cin >> B[i][j];
 				input_file >> B[i][j];
 			}
 		}
